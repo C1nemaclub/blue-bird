d04d49a4f1b0b75470ee9e26ab5b4b5f7e2bed81
C1nemaclub <santiagov801@gmail.com>
2023-10-07 17:25:59 -0500

    addedd test config and tests

---
+++ tests/math.spec.ts
@@ -0,0 +1,21 @@
+describe('Math', () => {
+  it('should add two numbers', () => {
+    const result = 1 + 2
+    expect(result).toBe(3)
+  })
+
+  it('should subtract two numbers', () => {
+    const result = 1 - 2
+    expect(result).toBe(-1)
+  })
+
+  it('should multiply two numbers', () => {
+    const result = 1 * 2
+    expect(result).toBe(2)
+  })
+
+  it('should divide two numbers', () => {
+    const result = 1 / 2
+    expect(result).toBe(0.5)
+  })
+})
